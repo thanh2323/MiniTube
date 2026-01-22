df5bedde86ed048b571aa7e19225a4b19c5734aa
thanh2323 <nguyenchithanh2213@gmail.com>
2026-01-22 15:12:32 +0700

    FEATURE: Implement global exception handling and result management

---
+++ BE/scr/MiniYoutube.Application/Shared/Error.cs
@@ -0,0 +1,7 @@
+namespace MiniYoutube.Application.Shared
+{
+    public sealed record Error(string Code, string Message)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+    }
+}
